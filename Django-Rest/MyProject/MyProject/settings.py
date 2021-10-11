0ab8e7e8c31ab9e1279d2ae244807fbdc909a095
Brian Su <briansu2004@hotmail.com>
2021-10-11 00:59:28 -0400

    Update settings.py

--- Django-Rest/MyProject/MyProject/settings.py
+++ Django-Rest/MyProject/MyProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hrq%5#9qynjc-5n*+y^aaitj6li72-crtew1$p!c_q_(i_vxu*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
