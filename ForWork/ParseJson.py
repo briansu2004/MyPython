15e866c5abc7a7d437bf5ac7770fe20b5ddf5a58
Brian Su <briansu2004@hotmail.com>
2023-03-17 16:41:57 -0400

    $

--- ForWork/ParseJson.py
+++ ForWork/ParseJson.py
@@ -1,14 +1,22 @@
 import json
 import os
 
-print(os.getcwd())  # prints the current working directory
+# Get the full path of the JSON file
+filename = os.path.join(os.getcwd(), 'ForWork', 'myrequest.json')
 
-# Load the JSON file
-with open('ForWork/myrequest.json') as f:
-    data = json.load(f)
+try:
+    # Load the JSON file
+    with open(filename) as f:
+        data = json.load(f)
 
-# Access the value of the "Data" key
-data_list = data['MyRequest']['Data']
+    # Access the value of the "Data" key
+    data_list = data['MyRequest']['Data']
 
-# Print the list of "Data" values
-print(data_list)
+    # Print the list of "Data" values
+    print(data_list)
+
+except FileNotFoundError:
+    print(f"Error: file {filename} not found.")
+
+except KeyError:
+    print(f"Error: 'MyRequest' field or 'Data' field not found in {filename}.")
