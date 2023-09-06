436f329dea1c4a02291f9e7875665b156bd75bbb
Brian Su <briansu2004@hotmail.com>
2023-09-06 16:24:07 -0400

    $

--- PySpark/PySparkReadLargeFile/PySparkReadLargeFile.py
+++ PySpark/PySparkReadLargeFile/PySparkReadLargeFile.py
@@ -1,31 +1,38 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import split, size
 
-## Part 1
+try:
+    ## Part 1
 
-spark = SparkSession.builder \
-    .appName("WordCount") \
-    .getOrCreate()
+    spark = SparkSession.builder \
+        .appName("WordCount") \
+        .getOrCreate()
 
-file_path = '/tmp/enwik9'
+    file_path = '/tmp/enwik9'
 
-# Read the text file into a DataFrame
-text_df = spark.read.text(file_path)
+    # Read the text file into a DataFrame
+    text_df = spark.read.text(file_path)
 
 
-## Part 2
+    ## Part 2
 
-# Split each line into words using whitespace as the delimiter
-words_df = text_df.select(split(text_df.value, " ").alias("words"))
+    # Split each line into words using whitespace as the delimiter
+    words_df = text_df.select(split(text_df.value, " ").alias("words"))
 
-# Count the number of words in each line
-word_count_df = words_df.select(size(words_df.words).alias("word_count"))
+    # Count the number of words in each line
+    word_count_df = words_df.select(size(words_df.words).alias("word_count"))
 
-# Calculate the total word count
-total_word_count = word_count_df.selectExpr("sum(word_count) as total_word_count").collect()[0]["total_word_count"]
+    # Calculate the total word count
+    total_word_count = word_count_df.selectExpr("sum(word_count) as total_word_count").collect()[0]["total_word_count"]
 
-print(f"Total words processed: {total_word_count}")
+    print(f"Total words processed: {total_word_count}")
 
-## Part 3
+    ## Part 3
 
-spark.stop()
+    spark.stop()
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+
+finally:
+    # Ensure Spark session is stopped
+    spark.stop()
